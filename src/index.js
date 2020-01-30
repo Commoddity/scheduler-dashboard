1b8df3fd42b663f015dbdd00696d5fd1f15a90a6
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-01-30 14:30:29 -0800

    Added CircleCI config files.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./index.scss";
+import './index.scss';
 
-import Dashboard from "components/Dashboard";
+import Dashboard from 'components/Dashboard';
 
-ReactDOM.render(<Dashboard />, document.getElementById("root"));
+import Axios from 'axios';
+
+if (process.env.REACT_APP_API_BASE_URL) {
+	Axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
